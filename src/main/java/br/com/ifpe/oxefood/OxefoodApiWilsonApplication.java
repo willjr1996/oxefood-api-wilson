a31f2e554e37cd02d047630da078250546740bf1
aluno <aluno@ifpe>
2024-10-10 21:24:51 -0300

    primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiWilsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiWilsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiWilsonApplication.class, args);
+	}
+
+}
