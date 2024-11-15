63d78785b891e5f57b4eae55092ddb53a0e8a9f3
aluno <aluno@ifpe>
2024-11-14 21:14:11 -0300

    implantação do método de listagem da tela de cliente

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,14 +1,15 @@
 package br.com.ifpe.oxefood.api.cliente;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 
@@ -17,12 +18,21 @@ import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 @CrossOrigin
 public class ClienteController {
     @Autowired
-   private ClienteService clienteService;
+    private ClienteService clienteService;
 
-   @PostMapping
-   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+    @PostMapping
+    public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+        Cliente cliente = clienteService.save(request.build());
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+    }
 
-       Cliente cliente = clienteService.save(request.build());
-       return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
-   }
+    @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterPorID(@PathVariable Long id) {
+        return clienteService.obterPorID(id);
+    }
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,19 +1,25 @@
 package br.com.ifpe.oxefood.modelo.cliente;
-
+import java.util.List;
 import jakarta.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
-   @Autowired
-   private ClienteRepository repository;
+    @Autowired
+    private ClienteRepository repository;
 
-   @Transactional
-   public Cliente save(Cliente cliente) {
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        cliente.setHabilitado(Boolean.TRUE);
+        return repository.save(cliente);
+    }
 
-       cliente.setHabilitado(Boolean.TRUE);
-       return repository.save(cliente);
-   }
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Cliente obterPorID(Long id) {
+        return repository.findById(id).get();
+    }
 }
