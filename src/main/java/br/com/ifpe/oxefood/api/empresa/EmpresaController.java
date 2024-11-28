6a0c776be6ab2495521b6b68409994c4866cb0b0
Wilson Barbosa <wilsonjunior_01@hotmail.com>
2024-11-28 11:32:28 -0300

    empresa

---
+++ src/main/java/br/com/ifpe/oxefood/api/empresa/EmpresaController.java
@@ -0,0 +1,52 @@
+package br.com.ifpe.oxefood.api.empresa;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+
+@RestController
+@RequestMapping("/api/cliente")
+@CrossOrigin
+public class EmpresaController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+        public ResponseEntity<Cliente> save(@RequestBody EmpresaRequest request) {
+        Cliente cliente = clienteService.save(request.build());
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+        public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+        public Cliente obterPorID(@PathVariable Long id) {
+        return clienteService.obterPorID(id);
+    }
+
+    @PutMapping("/{id}")
+        public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody EmpresaRequest request) {
+        clienteService.update(id, request.build());
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+       public ResponseEntity<Void> delete(@PathVariable Long id) {
+       clienteService.delete(id);
+       return ResponseEntity.ok().build();
+   }
+}
